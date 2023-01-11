6e67da1adc2c7c3fc5917ba412de4028f7d95878
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2023-01-11 16:52:32 -0300

    req: 01 - Feito.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Login() {
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [player, setPlayer] = useState({
+    name: '',
+    email: '',
+  });
+
+  const handleInputChange = ({ target }) => {
+    const { name, value } = target;
+    setPlayer({ ...player, [name]: value });
+  };
+
+  useEffect(() => {
+    const { name, email } = player;
+    if (name.length > 0 && email.length > 0) setIsDisabled(false);
+    else setIsDisabled(true);
+  }, [player]);
+
   return (
-    <div>Login</div>
+    <div>
+      <h1>Login</h1>
+      <form>
+        <input
+          type="text"
+          data-testid="input-player-name"
+          name="name"
+          onChange={ handleInputChange }
+        />
+        <input
+          type="text"
+          data-testid="input-gravatar-email"
+          name="email"
+          onChange={ handleInputChange }
+        />
+        <button
+          type="button"
+          data-testid="btn-play"
+          disabled={ isDisabled }
+        >
+          Play
+        </button>
+      </form>
+    </div>
   );
 }
 
