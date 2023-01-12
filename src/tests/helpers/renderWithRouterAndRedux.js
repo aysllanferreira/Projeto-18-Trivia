6dbe287d0572e6087fbfc21aca71b20be759ef9c
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2023-01-12 20:15:07 -0300

    req: 17 - feito

--- src/tests/helpers/renderWithRouterAndRedux.js
+++ src/tests/helpers/renderWithRouterAndRedux.js
@@ -3,10 +3,19 @@ import { render } from '@testing-library/react';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
-import store from '../../redux/store';
+import { configureStore } from '@reduxjs/toolkit';
+import player from '../../redux/reducers/player';
+import timer from '../../redux/reducers/timer';
 
 export const renderWithRouterAndRedux = (component, initialState, route = '/') => {
   const history = createMemoryHistory({ initialEntries: [route] });
+  const store = configureStore({
+    reducer: {
+      player,
+      timer,
+    },
+    preloadedState: initialState,
+  });
 
   return {
     ...render(
