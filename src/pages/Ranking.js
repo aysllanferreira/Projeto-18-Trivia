b2bd902b7ca728baada9c4b7598acc79734b82f0
¨Cassio <cassioschii@gmail.com>
2023-01-13 14:51:04 -0300

    Req18 - Feito

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 function Ranking() {
+  const history = useHistory();
   return (
     <div>
       <h1 data-testid="ranking-title">Ranking</h1>
+      <button
+        type="button"
+        data-testid="btn-go-home"
+        onClick={ () => history.push('/') }
+      >
+        Ir para a tela inicial
+      </button>
     </div>
   );
 }
